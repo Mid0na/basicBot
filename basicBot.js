00c1bfca863365fe78b0a9a0b619f475eb8f48d4
Mid0na <Mid0na@users.noreply.github.com>
2015-03-05 00:09:41 +0100

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1480,6 +1480,19 @@
                     }
                 }
             },
+            
+            bot.commands.baconCommand = {
+            command: 'lgdbot',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("/me Heroes do their best, Legends reach the top !");
+                }
+            }
+        },
 
             baCommand: {
                 command: 'ba',
@@ -2986,18 +2999,6 @@
                     }
                 }
             }
-            bot.commands.baconCommand = {
-            command: 'lgdbot',  //The command to be called. With the standard command literal this would be: !bacon
-            rank: 'user', //Minimum user permission to use the command
-            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
-            functionality: function (chat, cmd) {
-                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-                if (!bot.commands.executable(this.rank, chat)) return void (0);
-                else {
-                    API.sendChat("/me Heroes do their best, Legends reach the top !");
-                }
-            }
-        };
         }
     };
 
